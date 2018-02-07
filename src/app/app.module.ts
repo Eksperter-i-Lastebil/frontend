f64606258ba583ba15159c5f25c5f9e1f9045ab0
Christoffer Wilhelm Gran <cwgran@gmail.com>
2018-02-07 13:15:29 +0100

    Init frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCI_x52g169wIOv7HbXB-fb6fVObUCcr08'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
