5832d92c72e325221b360c390cae7f03d39a47d9
Christoffer Wilhelm Gran <cwgran@gmail.com>
2018-02-21 11:19:18 +0100

    Add location search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { MapsAPILoader } from '@agm/core';
+import { } from 'googlemaps';
+
 import { TripsService } from './trips.service';
 
 @Component({
@@ -6,13 +11,66 @@ import { TripsService } from './trips.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   trips = [];
 
-  constructor(private tripsService: TripsService){ 
-    this.tripsService.update().subscribe(data => this.trips = data);
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(private tripsService: TripsService,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone) {
+    //this.tripsService.update().subscribe(data => this.trips.push(data));
   };
+
+	ngOnInit() {
+    //set google maps defaults
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
+
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    //set current position
+    this.setCurrentPosition();
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["(cities)"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          console.log(this.latitude + " " + this.longitude);
+        });
+      });
+    });
+  }
+
+	private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    }
+  }
 
   toFloat(number) {
     return parseFloat(number);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
-
+import {FormsModule, FormControl, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
 import { TripsService } from './trips.service';
-
-
 
 @NgModule({
   declarations: [
@@ -16,8 +14,11 @@ import { TripsService } from './trips.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCI_x52g169wIOv7HbXB-fb6fVObUCcr08'
+      apiKey: 'AIzaSyCI_x52g169wIOv7HbXB-fb6fVObUCcr08',
+      libraries: ["places"]
     })
   ],
   providers: [TripsService],
